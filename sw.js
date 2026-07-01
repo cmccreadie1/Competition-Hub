e149feebfc5bbb79bc0b1b1a1cde861843485715
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-07-01 18:58:09 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// sw.js
+const CACHE_NAME = 'competition-hub-v7.4.0';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
